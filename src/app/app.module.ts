513b7e04438d13a486317b16f68d32b57942811e
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-16 21:37:47 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherDisplayComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
