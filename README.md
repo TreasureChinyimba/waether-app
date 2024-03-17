9a96887089f0a6d28c95f33aa47f344d74310d1b
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-03-17 14:13:12 +0200

    readme file

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+#Author
+
+Treasure Chinyimba
+
 # WeatherApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.0.
--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./weather-display.component.css']
 })
 export class WeatherDisplayComponent {
-  @Input() weatherData: any; 
+  @Input() weatherData: any;
 
   constructor() { }
 }
